66114d7797d4831fbf96063be9996903861370e7
sharadnawade <44015802+sharadnawade@users.noreply.github.com>
2018-11-22 16:36:16 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,1 @@
-node default {
-   file{'/root/README':
-   ensure  => file,
-   content => 'This is readme',
-   owner   => 'root',
-   }
-   file {'/root/README':
-           owner  => 'root',
-        }
-}
+
